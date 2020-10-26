23eb027dc95a6a9b0e747b9b3074236a68b04259
Usman Siddiqui <usman98789@hotmail.com>
2020-10-26 14:34:37 -0400

    adding src & public

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,101 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+	...SectionSplitProps.types,
+};
+
+const defaultProps = {
+	...SectionSplitProps.defaults,
+};
+
+const FeaturesSplit = ({
+	className,
+	topOuterDivider,
+	bottomOuterDivider,
+	topDivider,
+	bottomDivider,
+	hasBgColor,
+	invertColor,
+	invertMobile,
+	invertDesktop,
+	alignTop,
+	imageFill,
+	...props
+}) => {
+	const outerClasses = classNames(
+		"features-split section",
+		topOuterDivider && "has-top-divider",
+		bottomOuterDivider && "has-bottom-divider",
+		hasBgColor && "has-bg-color",
+		invertColor && "invert-color",
+		className
+	);
+
+	const innerClasses = classNames(
+		"features-split-inner section-inner",
+		topDivider && "has-top-divider",
+		bottomDivider && "has-bottom-divider"
+	);
+
+	const splitClasses = classNames(
+		"split-wrap",
+		invertMobile && "invert-mobile",
+		invertDesktop && "invert-desktop",
+		alignTop && "align-top"
+	);
+
+	const sectionHeader = {
+		title: "Our Apps",
+		// paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+	};
+
+	return (
+		<section {...props} className={outerClasses}>
+			<div className="container">
+				<div className={innerClasses}>
+					<SectionHeader data={sectionHeader} className="center-content" />
+					<div className={splitClasses}>
+						<div className="split-item">
+							<div
+								className="split-item-content center-content-mobile reveal-from-left"
+								data-reveal-container=".split-item"
+							>
+								<h3 className="mt-0 mb-12">Product Specific Age Verifier</h3>
+								<p className="m-0">
+									A paragraph of our age verifier app with button.
+								</p>
+							</div>
+							<div
+								className={classNames(
+									"split-item-image center-content-mobile reveal-from-bottom",
+									imageFill && "split-item-image-fill"
+								)}
+								data-reveal-container=".split-item"
+							>
+								<Image
+									src={require("./../../assets/images/features-split-image-01.png")}
+									alt="Features split 01"
+									width={528}
+									height={396}
+								/>
+							</div>
+						</div>
+
+						<h3 style={{ textAlign: "center" }} className="mt-0 mb-12">
+							More Apps Coming Soon...
+						</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
