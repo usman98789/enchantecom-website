7aa2280ad083362fd6c84cbf8b79549befd21483
Usman Siddiqui <usman98789@hotmail.com>
2021-11-14 23:27:46 -0500

    Updates

---
+++ precache-manifest.4d3c058ff2616afb9937e7a769c6da9d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef3a015e0e62e6495b9966693936a61",
+    "url": "https://enchantecom.com/index.html"
+  },
+  {
+    "revision": "001349ba679fc375c98f",
+    "url": "https://enchantecom.com/static/css/main.37794cae.chunk.css"
+  },
+  {
+    "revision": "3c47631c6f869310aeb5",
+    "url": "https://enchantecom.com/static/js/2.7d496823.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "https://enchantecom.com/static/js/2.7d496823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001349ba679fc375c98f",
+    "url": "https://enchantecom.com/static/js/main.f44e0fd8.chunk.js"
+  },
+  {
+    "revision": "711d72ff98c418fc4f8f",
+    "url": "https://enchantecom.com/static/js/runtime-main.b234aa98.js"
+  },
+  {
+    "revision": "24c9fe08cff9335a100f545572588319",
+    "url": "https://enchantecom.com/static/media/Farooq.24c9fe08.png"
+  },
+  {
+    "revision": "5fafdeed89b3e0aec6c27778facfee09",
+    "url": "https://enchantecom.com/static/media/Usman.5fafdeed.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "https://enchantecom.com/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "https://enchantecom.com/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "https://enchantecom.com/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "https://enchantecom.com/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "https://enchantecom.com/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "https://enchantecom.com/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "619f99ba33c4c0a4194dca6e9c540a24",
+    "url": "https://enchantecom.com/static/media/psav.619f99ba.PNG"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "https://enchantecom.com/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "8f8982ce98d33add89056e78f12fdc5e",
+    "url": "https://enchantecom.com/static/media/upw.8f8982ce.PNG"
+  }
+]);
